c891fd0f36d47858992e7516bbfecfba2477db4f
rakibinsky <rakibinsky@gmail.com>
2024-08-12 11:45:54 -0500

    Pushing new changes

--- pybasics.py
+++ pybasics.py
@@ -1,3 +1,4 @@
+#!/opt/homebrew/bin/python3
 
 # For loop example
 for i in range(3):
@@ -11,11 +12,27 @@ while count < 5:
     count += 1
 
 # Conditionals
-
 age = int(input("Enter the age: "))
-if age >= 18 and age <= 150:
+if age > 18 and age < 150:
     print("You are an adult.")
 elif age > 150:
-    print("RIP")
+    print("R.I.P")
 else:
     print("You are a minor.")
+
+# Functions
+def add_numbers(num1, num2):
+    return num1 + num2
+
+def sub_numbers(num1, num2):
+    return num1 - num2
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+Summation = int (add_numbers(num1,num2))
+Subtraction = int (sub_numbers(num1,num2))
+
+print("Sum of the numbers is: ", Summation)
+print("Sub of the numbers is: ", Subtraction)
+
