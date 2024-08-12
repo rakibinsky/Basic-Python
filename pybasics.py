106fd725d43b3286712f292f7640b85e8576bb52
rakibinsky <rakibinsky@gmail.com>
2024-08-12 10:50:03 -0500

    Pushing updates for pybasics

--- pybasics.py
+++ pybasics.py
@@ -1,7 +1,21 @@
-for i in range(3):
-    print(i)
 
+# For loop example
+for i in range(3):
+    print("Range from for loop: ", i)
+
+
+# While loop example
 count = 0
 while count < 5:
-    print(count)
+    print("Range from while loop: ", count)
     count += 1
+
+# Conditionals
+
+age = int(input("Enter the age: "))
+if age >= 18 and age <= 150:
+    print("You are an adult.")
+elif age > 150:
+    print("RIP")
+else:
+    print("You are a minor.")
